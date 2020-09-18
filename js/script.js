0a2526b879d48b086a1fb76455f14ba387c3e161
Leanne Heuberger <leanneheuberger@gmail.com>
2020-09-18 11:30:27 +0200

    Completed project
    - Added function getRandomQuote
    - Added function printQuote
    - Added quotes and detailed information to object named quotes

--- js/script.js
+++ js/script.js
@@ -3,19 +3,26 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance:
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+/*
+  The code below will generate a random quote selected from an object (named quotes) containing an array of various quotes by famous feminists.
+  The randomly selected quote will display: 1. The quote itself and 2. The name of the person who stated the quote. For some quotes, there is
+  additional information about the quote, namely in what context it was stated (value for 'citation') and in what year it was stated.
 
-/***
- * `quotes` array
-***/
+  This code has the following set-up:
+      - Section 1: The object 'quotes' and the string-variable html are defined;
+      - Section 2: The two functions (called getRandomQuote and printQuote) are defined;
+      - Section 3: The values of the randomly selected quote (stored in the variable html) are printed to the web page using the ID 'quote-box'.
 
+  If something is unclear in this code, please contact me.
+*/
+
+// Section 1:
+// quotes-object with 5 different quotes by famous feminists:
 const quotes = [
   {
     quote: `To all the little girls who are watching this, never doubt that you are valuable and powerful, and deserving of every chance and opportunity in the world to pursue and achieve your own dreams.`,
     source: 'Hilary Clinton',
-    citation: 'Concession Speach',
+    citation: 'Concession Speech',
     year: 2016
   },
   {
@@ -39,19 +46,36 @@ const quotes = [
     year: 2014
   }
 ];
-console.log(quotes);
 
-/***
- * `getRandomQuote` function
-***/
+let html = '';
 
+//Section 2:
+// getRandomQuote-function:
+function getRandomQuote() {
+  let num = Math.floor( Math.random() * quotes.length ); // The num-variable stores a random number ranging from 0 to the index of the last item in the quotes array, in this case: 4.
+  return quotes[num];
+}
+getRandomQuote();
 
+// printQuote-function:
+function printQuote() {
+  let myQuote = getRandomQuote(); //variable to store a random quote object from the getRandomQuote() function
+  html += `<p class="quote">${myQuote.quote}</p><p class="source">${myQuote.source}`; //variable to store the HTML string.
+  if (myQuote.citation !== '') {
+    html += `<span class="citation">${myQuote.citation}</span>`;
+  }
+  if (myQuote.year !== '') {
+    html += `<span class="year">${myQuote.year}</span>`;
+  }
+  html += `</p>`;
+  // console.log(html);
+  return html;
+}
+printQuote();
 
-/***
- * `printQuote` function
-***/
-
-
+//Section 3:
+//The content of the string-variable html is pushed to the web page.
+document.getElementById('quote-box').innerHTML = html;
 
 /***
  * click event listener for the print quote button
@@ -59,9 +83,3 @@ console.log(quotes);
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
-
-/*
-  Notes Leanne: Must create at least 5 quotes (or objects)
-  For this project, I will need two functions: A 'getRandomQuote'-function and a 'printQuote'-function
-
-*/
